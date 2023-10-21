85284230eb1962516d17597c2607df66574db308
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2023-10-21 08:27:21 +0200

    Navbar&Products

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+type ButtonType = {
+    children: React.ReactNode;
+    classname?: string
+    onClick? : () => void
+}
+
+const Button = ({children, classname}: ButtonType)  => {
+  return (
+    <button onClick={() => {}} className={`py-2 px-2 bg-black text-white rounded-md shadow-md hover:opacity-75 ${classname}`}>
+      <h1>{children}</h1>
+    </button>
+  )
+}
+
+export default Button
